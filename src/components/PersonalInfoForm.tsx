558368c1a5838d53ac0b5787b499ddb7e5cdb765
Kamran Davar <kamran.davar.KD@GMAIL.COM>
2023-11-14 00:53:20 +0330

    style: more detail in styles

--- src/components/PersonalInfoForm.tsx
+++ src/components/PersonalInfoForm.tsx
@@ -7,10 +7,9 @@ function PersonalInfoForm() {
         تکمیل اطلاعات
       </header>
       <section className="flex w-full flex-col mt-9 mb-5 px-5">
-        <div className="text-black text-right text-base leading-7 max-w-[346px]">
-          <p>لطفا اطلاعات شخصی مالک خودرو را وارد کنید</p>
-        </div>
-        <div className=" self-stretch shrink-0 h-px mt-4" />
+        <p className="text-black text-right text-base leading-7 max-w-[346px]">
+          لطفا اطلاعات شخصی مالک خودرو را وارد کنید
+        </p>
         <input
           type="text"
           id="nationalId"
@@ -23,17 +22,20 @@ function PersonalInfoForm() {
           className="text-neutral-500 text-right text-sm font-medium leading-5 whitespace-nowrap border bg-white self-stretch mt-6 px-5 py-5 border-solid border-zinc-400 max-md:pr-0"
           placeholder="شماره تلفن همراه"
         />
-        <div className="text-black text-right text-base font-semibold leading-7 whitespace-nowrap mt-8">
-          <p>آدرس جهت درج روی بیمه نامه</p>
-        </div>
-        <section className="text-black text-right text-sm leading-7 mt-2.5">
-          <p>لطفا آدرسی را که می‌خواهید روی بیمه‌نامه درج شود، وارد کنید.</p>
-        </section>
-        <div className="text-black text-center text-base font-semibold leading-6 whitespace-nowrap bg-amber-300 self-center w-full max-w-xs items-center mt-8 px-5 py-4">
-          <p>انتخاب از آدرس‌های من</p>
-        </div>
+        <p className="text-black text-right text-base font-semibold leading-7 whitespace-nowrap mt-8">
+          آدرس جهت درج روی بیمه نامه
+        </p>
+        <p className="text-black text-right text-sm leading-7 mt-2.5">
+          لطفا آدرسی را که می‌خواهید روی بیمه‌نامه درج شود، وارد کنید.
+        </p>
+        <button className="text-black text-center text-base font-semibold leading-6 whitespace-nowrap bg-amber-300 self-center w-full items-center mt-8 px-5 py-4">
+          انتخاب از آدرس‌های من
+        </button>
         <div>
-          <button className="bg-[black] text-[white] rounded text-center cursor-pointer mt-7 py-2 px-5 w-auto">
+          <button
+            type="submit"
+            className="bg-[black] text-[white] rounded text-center cursor-pointer mt-7 py-2 px-5 w-auto"
+          >
             تایید و ادامه
           </button>
         </div>
