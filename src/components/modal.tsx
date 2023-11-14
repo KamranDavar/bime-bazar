d0bc0b0eb53942922a3e11748674454d93abbe0f
Kamran Davar <kamran.davar.KD@GMAIL.COM>
2023-11-15 00:31:52 +0330

    feat: add more styles to modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { MouseEvent, useState } from "react";
-import { AiOutlineClose } from 'react-icons/ai';
-
+import { AiOutlineClose } from "react-icons/ai";
 
 function Modal() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleModal = (e: any) => {
+  const toggleModal = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setIsOpen(!isOpen);
   };
 
-  const handleBlurClick = (e: any) => {
-    if (isOpen && e.target.classList.contains("bg-blur")) {
+  const handleBlurClick = (e:  MouseEvent<HTMLDivElement>) => {
+    const targetElement = e.target as HTMLElement;
+    if (isOpen && targetElement.classList.contains("bg-blur")) {
       setIsOpen(false);
     }
   };
@@ -28,21 +28,20 @@ function Modal() {
 
       {isOpen && (
         <div
-          className="fixed inset-0 flex items-center justify-center bg-blur"
+          className="fixed inset-0 flex items-end sm:items-center justify-center bg-blur"
           onClick={handleBlurClick}
         >
-          <section className="bg-white flex flex-col items-stretch min-w-sm-full  min-w-md-[386px] px-5">
-            <header className="items-center flex w-full flex-col mt-6 pb-2 px-5">
-              <div className="justify-between items-stretch flex gap-5">
-                <AiOutlineClose onClick={toggleModal}/>
+          <section className="bg-white flex flex-col items-stretch min-w-full sm:min-w-fit  h-fit">
+            <header className="items-center flex w-full flex-col mt-6 pb-2  rounded-t-lg px-5 border-b border-gray-200">
+              <div className="flex justify-between gap-5 w-full">
+                <AiOutlineClose onClick={toggleModal} />
                 <h1 className="text-black text-right text-base font-medium leading-6 whitespace-nowrap mr-0">
-                  Address Selection
+                  انتخاب آدرس
                 </h1>
               </div>
-              <hr className="bg-neutral-200 self-stretch min-h-[1px] w-full -mr-5 mt-3" />
             </header>
-            <div className="self-stretch flex w-full justify-between gap-4 -mr-5">
-              <div className="flex grow basis-[0%] flex-col mt-5">
+            <div className="self-stretch flex w-full justify-between gap-4 -mr-5 px-5">
+              <div className="flex grow basis-[0%] flex-col my-5">
                 <div className="flex w-48 max-w-full gap-3 pr-1">
                   <h2 className="text-black text-right text-sm font-medium leading-5">
                     قرش نانیرفآ‌شقن تکرش
@@ -72,9 +71,9 @@ function Modal() {
                 </p>
               </div>
             </div>
-            <footer className="shadow-sm bg-white flex w-full flex-col items-stretch mt-16 p-5 max-md:mt-10">
-              <button className="text-white text-center text-base font-medium leading-7 whitespace-nowrap bg-black items-center px-5 py-3.5">
-                Select
+            <footer className="flex w-full flex-col items-stretch p-5 border-t border-gray-300 ">
+              <button className="text-white text-center bg-black items-center px-5 py-2.5">
+                انتخاب
               </button>
             </footer>
           </section>
